ce417ef31a72fd5ff06c9a4ead1a8396db8e5413
Mateo Buitrago <mateobuitrago@Cesars-Mac-mini.local>
2023-08-08 17:35:53 -0400

    server on

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/home.html');
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(__dirname + '/about.html');
+});
+
+app.get('/contact-us', (req, res) => {
+  res.sendFile(__dirname + '/contact-us.html');
+});
+
+app.listen(port, () => {
+  console.log(`Servidor corriendo en http://localhost:${port}`);
+});
